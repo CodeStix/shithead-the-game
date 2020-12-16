9c62d04fb0d0f4c3922486f58ef03d22f407da81
Stijn Rogiest <reddusted@gmail.com>
2020-12-16 20:07:09 +0100

    Prettier

--- server/cardEngineServer.js
+++ server/cardEngineServer.js
@@ -4,23 +4,21 @@ const Game = require("./Game");
 var players = [];
 var games = [new Game("game0", 4)];
 var anyGameIndex = 0;
+console.log("starting websocket server");
 
-const server = new WebSocket.Server({port: 81});
+const server = new WebSocket.Server({ port: 81 });
 server.on("connection", (socket) => {
+    console.log("connection");
 
-    if (socket.protocol !== "cards")
-    {
+    if (socket.protocol !== "cards") {
         socket.close(1000, "unsupported protocol");
         return;
     }
 
     socket.on("close", (code, reason) => {
-
         var player = players.find((pl) => pl.socket === socket);
-        if (player)
-        {
-            if (player.joinedGame)
-            {
+        if (player) {
+            if (player.joinedGame) {
                 console.log("leaving game due to socket close...", player.name);
                 player.joinedGame.letLeave(player);
             }
@@ -28,23 +26,18 @@ server.on("connection", (socket) => {
     });
 
     socket.on("message", (message) => {
-
         var commands = message.split("|");
         console.log("received: %s", commands);
-        
-        for(let i = 0; i < commands.length; i++)
-        {
+
+        for (let i = 0; i < commands.length; i++) {
             var command = commands[i].trim();
-            if (command.length === 0)
-                continue;
+            if (command.length === 0) continue;
             var args = command.split(" ");
             var player = players.find((pl) => pl.socket === socket);
 
-            switch(args[0]) 
-            {
+            switch (args[0]) {
                 case "gamestate":
-                    if (!player || !player.joinedGame)
-                    {
+                    if (!player || !player.joinedGame) {
                         console.warn("game or player does not exist");
                         continue;
                     }
@@ -52,8 +45,7 @@ server.on("connection", (socket) => {
                     continue;
 
                 case "gamestatevote":
-                    if (!player || !player.joinedGame)
-                    {
+                    if (!player || !player.joinedGame) {
                         console.warn("game or player does not exist");
                         continue;
                     }
@@ -61,25 +53,21 @@ server.on("connection", (socket) => {
                     continue;
 
                 case "setplayer":
-                    if (player)
-                    {
+                    if (player) {
                         player.name = args[1];
                         continue;
                     }
                     var existingPlayer = players.find((pl) => pl.name == args[1]);
-                    if (existingPlayer)
-                    {
+                    if (existingPlayer) {
                         console.log("player overtook lingering socket", existingPlayer.name);
                         existingPlayer.socket.close();
                         existingPlayer.socket = socket;
                         continue;
-                    }    
-                    else
-                    {
+                    } else {
                         player = {
                             name: args[1],
                             socket: socket,
-                            joinedGame: null
+                            joinedGame: null,
                         };
                         players.push(player);
                     }
@@ -87,19 +75,16 @@ server.on("connection", (socket) => {
 
                 case "joinany":
                     var game = games[anyGameIndex];
-                    if (!game || !player)
-                    {
+                    if (!game || !player) {
                         console.warn("game or player does not exist");
                         socket.close(1011, "game or player doesn't exist");
                         break;
                     }
-                    if (player.joinedGame !== null)
-                    {
+                    if (player.joinedGame !== null) {
                         console.warn("player already in a game, leaving previous game...", player.name);
                         player.joinedGame.letLeave(player);
                     }
-                    if (game.inGame() || !game.letJoin(player))
-                    {
+                    if (game.inGame() || !game.letJoin(player)) {
                         console.warn("cannot join, creating new game...");
                         game = new Game("game" + ++anyGameIndex, 4);
                         games.push(game);
@@ -110,39 +95,33 @@ server.on("connection", (socket) => {
 
                 case "join":
                     var game = games[parseInt(args[1])];
-                    if (!game || !player)
-                    {
+                    if (!game || !player) {
                         console.warn("game or player does not exist");
                         socket.close(1011, "game or player doesn't exist");
                         break;
                     }
-                    if (game.inGame())
-                    {
+                    if (game.inGame()) {
                         socket.close(1011, game.name + " already started");
                         break;
                     }
-                    if (player.joinedGame !== null)
-                    {
+                    if (player.joinedGame !== null) {
                         console.warn("player already in a game, leaving previous game...", player.name);
                         player.joinedGame.letLeave(player);
                     }
-                    if (!game.letJoin(player))
-                    {
+                    if (!game.letJoin(player)) {
                         console.warn(player.name, "cannot join", game.name);
                         socket.close(1011, "cannot join room " + game.name);
                         break;
                     }
                     socket.send("setmaster " + game.players[0].name);
                     continue;
-                    
+
                 case "leave":
-                    if (!player || !player.joinedGame)
-                    {
+                    if (!player || !player.joinedGame) {
                         console.warn("cannot leave nothing");
                         continue;
                     }
-                    if (!player.joinedGame.letLeave(player))
-                    {
+                    if (!player.joinedGame.letLeave(player)) {
                         console.warn("could not leave");
                     }
                     continue;
@@ -151,18 +130,15 @@ server.on("connection", (socket) => {
                     socket.close();
                     continue;
 
-                case "broadcastall":   
+                case "broadcastall":
                 case "broadcast":
                     var includeSender = args[0] === "broadcastall";
-                    if (!player || !player.joinedGame)
-                    {
+                    if (!player || !player.joinedGame) {
                         console.warn("cannot broadcast in nothing");
                         continue;
                     }
-                    for(let j = 0; j < player.joinedGame.players.length; j++)
-                    {
-                        if (includeSender || player.joinedGame.players[j] !== player)
-                            player.joinedGame.players[j].socket.send(args.slice(1).join(" "));
+                    for (let j = 0; j < player.joinedGame.players.length; j++) {
+                        if (includeSender || player.joinedGame.players[j] !== player) player.joinedGame.players[j].socket.send(args.slice(1).join(" "));
                     }
                     continue;
 
@@ -171,7 +147,6 @@ server.on("connection", (socket) => {
                     continue;
             }
         }
-
     });
 
     console.log("connection was made.", socket.protocol);
